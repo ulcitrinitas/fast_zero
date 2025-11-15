8e70edda53d071444ba05cdc36a47a0d5a04a92a
Ulisses Silva <uli.citrinitas@gmail.com>
2025-11-15 18:19:39 -0300

    adicionada get para users

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -2,7 +2,7 @@ from http import HTTPStatus
 from fastapi import FastAPI
 
 from fast_zero.schemas import Message
-from fast_zero.schemas import UserSchema, UserPublic, UserDB
+from fast_zero.schemas import UserSchema, UserPublic, UserDB, UserList
 
 app = FastAPI()
 
@@ -12,6 +12,9 @@ database = list()
 def read_root():
     return {"message": "Rodando no fastapi"}
 
+@app.get("/users/", status_code=HTTPStatus.OK, response_model=UserList)
+def read_users():
+    return {'users': database}
 
 @app.post("/users/", status_code=HTTPStatus.CREATED, response_model=UserPublic)
 def create_user(user: UserSchema):

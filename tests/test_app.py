7feb5f95eee6c4a586f3d69ad484b4fd7727f594
Ulisses Silva <uli.citrinitas@gmail.com>
2025-11-15 17:54:34 -0300

    criado teste de criação do usuario

--- tests/test_app.py
+++ tests/test_app.py
@@ -10,3 +10,20 @@ def test_root_deve_retornar_uma_msg_e_um_ok():
     
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {"message": "Rodando no fastapi"}
+
+
+def test_criar_usuario():
+    client = TestClient(app)
+    
+    response = client.post("/users/", json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'secret'
+        })
+    
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'alice',
+        'email': 'alice@example.com',
+        'id': 1
+    }

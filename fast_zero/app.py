ecfcd0f6a384a63cf2a479b4f35a0f685b070e78
Ulisses Silva <uli.citrinitas@gmail.com>
2025-11-15 17:51:16 -0300

    adcionada rota para criar usuario

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,10 +1,12 @@
 from http import HTTPStatus
 from fastapi import FastAPI
 
-from fast_zero.schemas import Message, UserSchema, UserPublic
+from fast_zero.schemas import Message
+from fast_zero.schemas import UserSchema, UserPublic, UserDB
 
 app = FastAPI()
 
+database = list()
 
 @app.get("/", status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
@@ -13,4 +15,11 @@ def read_root():
 
 @app.post("/users/", status_code=HTTPStatus.CREATED, response_model=UserPublic)
 def create_user(user: UserSchema):
-    ...
+    
+    user_with_id = UserDB(**user.model_dump(), id=(len(database) + 1))
+    
+    database.append(user_with_id)
+    
+    print(database)
+    
+    return user_with_id

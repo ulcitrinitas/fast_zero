b423462ddc9750a65bb76278c02cc8e523cde7d2
Ulisses Silva <uli.citrinitas@gmail.com>
2025-11-21 20:08:50 -0300

    criada configuração do banco de dados

--- fast_zero/models.py
+++ fast_zero/models.py
@@ -1,4 +1,23 @@
 from datetime import datetime
-from sqlalchemy.orm import Mapped, mapped_as_dataclass, registry
+from uuid import UUID
+
+from sqlalchemy import func
+from sqlalchemy.orm import Mapped, mapped_as_dataclass, mapped_column, registry
+
+table_registry = registry()
 
 
+@mapped_as_dataclass(table_registry)
+class User:
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(
+        init=False, primary_key=True, autoincrement=True
+    )
+    username: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[str]
+    email: Mapped[str] = mapped_column(unique=True)
+    guid: Mapped[UUID]
+    created_at: Mapped[datetime] = mapped_column(
+        init=False, server_default=func.now()
+    )
